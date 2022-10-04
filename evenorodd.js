642add289055c8412ed4c0e23cb3988804cc9ee7
tyana bandeira <tyanabandeira1@gmail.com>
2022-10-04 15:52:41 -0400

    Even or Odd 8kyu

---
+++ evenorodd.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
+even_or_odd(4)
