318ebc5429ae303c1b473d2c3d34002344f46892
tyana bandeira <tyanabandeira1@gmail.com>
2022-10-18 00:45:23 -0400

    stringRepeat

---
+++ stringRepeat.js
@@ -0,0 +1,18 @@
+function repeatStr (n, s) {
+  let string = ''
+  for(let i=0; i < n; i++){
+    string += s 
+  }
+ return string;``
+}
+
+/** 
+p parameter - input is s(string) and n(integer)
+r return - return string(s) repeated n(integer) times
+e - 10, 'a' > 'aaaaaaaaaa'
+p-  psuedocode 
+1. function repeatStr is given
+2. function is named repeatStr
+3. passing a parameter of n(integer) and s (string)
+4. need to return a string repeated n times
+5. mathematical operation */
