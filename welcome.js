e6e8bb1f6a94ae5a5f2bd49bbc46368edef77c96
tyana bandeira <tyanabandeira1@gmail.com>
2023-01-09 22:11:12 -0500

    pushing codewar

---
+++ welcome.js
@@ -0,0 +1,26 @@
+function greet(language) {
+  const languageDB = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+}
+	 if (languageDB[language]) {
+    return languageDB[language];
+  } else {
+    return languageDB['english'];
+  }
+}
